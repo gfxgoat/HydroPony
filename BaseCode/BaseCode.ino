79c59e45981b645110bd5c6e17058a93ab9d5d7e
Gordon Cherrett <nibler@mac.com>
2014-08-25 22:26:17 +0100

    Changed Analog pins to A0 - 3 and added UDP
    Changed Analog pins to A0 - 3 and added UDP
    
    Minor error when moving message construction(UDP) block broke something
    but need to sleep =(
    
    Added line to read me

--- BaseCode/BaseCode.ino
+++ BaseCode/BaseCode.ino
@@ -4,7 +4,7 @@
 //<<<<<<< HEAD
 // --- Global Variables ---
 const int countTemp = 4; // Only 3 sensors for starters, done this way to make it flexible in the loop below
-int pinTemp [countTemp] = {15,16,17,18}; //pins for sensors
+int pinTemp [countTemp] = {A0,A1,A2,A3}; //pins for sensors
 int sensorTemp [countTemp]; //temp sensor readings
 int sensorTimer = 10; //ms
 int count = 0;
@@ -17,9 +17,8 @@ int count = 0;
 #include <EthernetUdp.h> // adds UPD support
 //Temp sensor code and tutorial  https://learn.adafruit.com/tmp36-temperature-sensor
 
-//TMP36 Attached to Pin XX - this is for reading the temp in the fish pond
-int tempSen1Pin = 0;
-
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+char  ReplyBuffer[] = "acknowledged";       // a string to send back
 //Setting for network interface 
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
@@ -28,6 +27,8 @@ byte mac[] = {
 IPAddress ip(192, 168, 0, 88);
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
+// traget server address
+IPAddress targetServer(192, 168, 0, 89);
 // port for sending data
 unsigned int localPort = 8888; 
 // An EthernetUDP instance to let us send and receive packets over UDP
@@ -58,47 +59,40 @@ void loop() {
   
   --- Process ---
   
+  Set message back to the server
   */
+
+ 
   
 for (count=0;count=countTemp;count++) {
  Serial.print(sensorTemp[count]); Serial.println(" degrees C"); //to degrees ((voltage - 500mV) times 100)
 }
- delay(1000);  // debug delay so we have time to read the serial feed
+ delay(100);  // debug delay so we have time to read the serial feed
  
- /* exaple code for reciving udp message maybe.
- int packetSize = Udp.parsePacket();
+// Below comment 5 Lines, unsure why as compiled be for moving broke with copy and pasting time for bed =(
+// String webMessage; // This will be the message returned to the server with the current status
+//Writing the UDP packet with our string for the web server to eat
+//  Udp.beginPacket(targetServer, localPort );  
+//  Udp.write(webMessage);
+//  Udp.endPacket();
+ 
+ 
+ // exaple code for reciving udp message maybe.
+int packetSize = Udp.parsePacket();
   if (packetSize)
   {
     Serial.print("Received packet of size ");
     Serial.println(packetSize);
-    Serial.print("From ");
-    IPAddress remote = Udp.remoteIP();
-    for (int i = 0; i < 4; i++)
-    {
-      Serial.print(remote[i], DEC);
-      if (i < 3)
-      {
-        Serial.print(".");
-      }
-    }
-    Serial.print(", port ");
-    Serial.println(Udp.remotePort());
 
     // read the packet into packetBufffer
     Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
-    Serial.println("Contents:");
+    Serial.print("Contents: ");
     Serial.println(packetBuffer);
- */
- 
- 
- /* example code for writing UDP packet 
-  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());  
-  Udp.write(ReplyBuffer);
-  Udp.endPacket();
- */
- 
+   // Ok we got a incoming message do somthing with it
+   
+  // brake down the message and then call somthing to happen 
+  }
 }
-
 void checkTemp () {
   for (count=0;count=countTemp;count++) {
    sensorTemp[count] = analogRead(pinTemp[countTemp]);//first sensor read - dump this as per adafruit guide 
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 HydroPony
 =========
 
+A small project to create a aquponics system in Sams garden and expand our knowledge.
+
 Hydroponics Code
