8e2b10e9c659681087f6433c808d70375157d4be
Gordon Cherrett <nibler@mac.com>
2014-08-25 18:12:27 +0100

    Adding Support for UDP interface
    Adding Support for UDP interface

--- BaseCode/BaseCode.ino
+++ BaseCode/BaseCode.ino
@@ -1,18 +1,37 @@
 //HydroPony 25/10/14
 //Building for Mega2560
 
+//including ethernet library
+#include <Ethernet.h>
+#include <EthernetUdp.h> // adds UPD support
 //Temp sensor code and tutorial  https://learn.adafruit.com/tmp36-temperature-sensor
 
 //TMP36 Attached to Pin XX - this is for reading the temp in the fish pond
 int tempSen1Pin = 0;
 
+//Setting for network interface 
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+// assign an IP address for the controller:
+IPAddress ip(192, 168, 0, 88);
+IPAddress gateway(192, 168, 0, 1);
+IPAddress subnet(255, 255, 255, 0);
+// port for sending data
+unsigned int localPort = 8888; 
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
 
 void setup() {
   // put your setup code here, to run once:
   
   //Start the serial interface (this will be removed once we've added the network shield)
   Serial.begin(9600);
-
+  // start ethernet
+ Ethernet.begin(mac, ip);
+ Udp.begin(localPort);
+  // delay for ethernet to come up
+ delay(1000);
 }
 
 void loop() {
@@ -41,4 +60,37 @@ You can fix this by doing two delayed readings and tossing out the first one */
  Serial.print(temperatureC); Serial.println(" degrees C"); //to degrees ((voltage - 500mV) times 100)
   
  delay(1000);  // debug delay so we have time to read the serial feed
+ 
+ /* exaple code for reciving udp message maybe.
+ int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i = 0; i < 4; i++)
+    {
+      Serial.print(remote[i], DEC);
+      if (i < 3)
+      {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+ */
+ 
+ 
+ /* example code for writing UDP packet 
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());  
+  Udp.write(ReplyBuffer);
+  Udp.endPacket();
+ */
+ 
 }
